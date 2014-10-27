fa60b8fa345698e3e9761010f8be09736fc074d1
Mike <mike.spelling@hotmail.co.uk>
2014-10-27 14:26:36 +0000

    Create basic crud operations for a comment

--- dynomike/config/routes.rb
+++ dynomike/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'admin#index'
+
+  get 'admin/index'
+
+  resources :comments
+  delete "comments" => "comments#delete_all"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
